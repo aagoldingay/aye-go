8f1d9f22adcf7b58b9cfa3c0302bfa734c4c34aa
aagoldingay <aarongoldingay@hotmail.com>
2019-03-14 18:24:58 +0000

    gitignore and some startup logic

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/globalsign/mgo"
+)
 
 func main() {
+	_, err := mgo.Dial("localhost:27017")
+	if err.Error() == "no reachable servers" {
+		fmt.Println("sorted")
+		os.Exit(1)
+	}
+	// try reading a config.txt, if not exist, create one + db (if not exists.....)
 	fmt.Println("aye-go")
 }
