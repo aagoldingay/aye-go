cc0f3d2d03537ae0dec0c605641b59a6acf4507e
aagoldingay <aarongoldingay@hotmail.com>
2019-04-27 01:49:28 +0100

    updated css

--- static/js/electionSetup.js
+++ static/js/electionSetup.js
@@ -3,7 +3,8 @@ function addOption() {
     var current = parseInt(numOptions.value);
     current++;
     document.getElementById("options").insertAdjacentHTML("beforeend", 
-        "<input type=\"text\" name=\"option-" + current + "\"/><br>");
+        "<input type=\"text\" class=\"form-control\" name=\"option-" + current + "\"/>");
+    // "<input type=\"text\" name=\"option-" + current + "\"/><br>");
     numOptions.value = current;
 }
 
@@ -11,10 +12,10 @@ function addOption() {
 function preSubmit() {
     if (document.getElementById("newelection") != null) {
         var frm = document.getElementById("form");
-        var title = frm.children[1].value;
-        var startdate = frm.children[3].value;
-        var enddate = frm.children[5].value;
-        var opts = frm.children[8];
+        var title = frm.children[1].children[0].value;
+        var startdate = frm.children[2].children[0].value;
+        var enddate = frm.children[3].children[0].value;
+        var opts = frm.children[5];
         if (title == "" || startdate == "" || enddate == "") {
             return false;
         }
--- static/js/register.js
+++ static/js/register.js
@@ -32,24 +32,36 @@ function register() {
     var frm = document.getElementById("form")
     frm.setAttribute("onsubmit", "return preSubmit();")
     frm.innerHTML = "<div class=\"apikey\">"
-        + "Integration ID:  <input type=\"text\" name=\"part-1\" maxlength=\"5\" size=\"5\" required> - <input type=\"text\" name=\"part-2\" maxlength=\"5\" size=\"5\" required> - <input type=\"text\" name=\"part-1\" maxlength=\"5\" size=\"5\" required>"
-        + "</div>" 
-        + "Username (remember this): <input type=\"text\" name=\"username\" value=\"" + generateShortID(8) + "\" readonly/><br>"
-        + "Password: <input type=\"password\" name=\"password\"/><br>"
-        + "Confirm Password: <input type=\"password\" name=\"confirmpassword\"/><br>"
-        + "Safeword: <input type=\"password\" name=\"safeword\"/> (used to prove you were not coerced when voting)</br>"
-        + "Confirm Safeword: <input type=\"password\" name=\"confirmsafeword\"/><br>"
-        + "<input type=\"hidden\" name=\"method\" value=\"register\"/>"
-        + "<input type=\"submit\" value=\"Submit\"/>";
+    + "Integration ID: <div class=\"form-row\">"
++ "<div class=\"col-md-3\"> <input type=\"text\" class=\"form-control\" name=\"part-1\" maxlength=\"5\" size=\"5\" required> </div> - <div class=\"col-md-3\"> <input type=\"text\" class=\"form-control\" name=\"part-2\" maxlength=\"5\" size=\"5\" required> </div> - <div class=\"col-md-3\"> <input type=\"text\" class=\"form-control\" name=\"part-1\" maxlength=\"5\" size=\"5\" required> </div>"
++ "</div></div><br>"
++ "<div class=\"form-group w-25\">Username (remember this): <input type=\"text\" class=\"form-control\" name=\"username\" value=\"" + generateShortID(8) + "\" readonly/> </div>"
++ "<div class=\"form-group w-25\">Password: <input type=\"password\" class=\"form-control\" name=\"password\"/> </div>"
++ "<div class=\"form-group w-25\">Confirm Password: <input type=\"password\" class=\"form-control\" name=\"confirmpassword\"/> </div>"
++ "<div class=\"form-group w-25\">Safeword: <div class=\"form-row\"><input type=\"password\" class=\"form-control\" name=\"safeword\"/> </div> <p><small>(used to prove you were not coerced when voting)</small></p></div>"
++ "<div class=\"form-group w-25\">Confirm Safeword: <input type=\"password\" class=\"form-control\" name=\"confirmsafeword\"/> </div>"
++ "<input type=\"hidden\" name=\"method\" value=\"register\"/>"
++ "<input type=\"submit\" class=\"btn btn-success btn-lg active\" aria-pressed=\"true\" value=\"Submit\"/>";
+        // + "Integration ID: <input type=\"text\" name=\"part-1\" maxlength=\"5\" size=\"5\" required> - <input type=\"text\" name=\"part-2\" maxlength=\"5\" size=\"5\" required> - <input type=\"text\" name=\"part-1\" maxlength=\"5\" size=\"5\" required>"
+        // + "</div>" 
+        // + "Username (remember this): <input type=\"text\" name=\"username\" value=\"" + generateShortID(8) + "\" readonly/><br>"
+        // + "Password: <input type=\"password\" name=\"password\"/><br>"
+        // + "Confirm Password: <input type=\"password\" name=\"confirmpassword\"/><br>"
+        // + "Safeword: <input type=\"password\" name=\"safeword\"/> (used to prove you were not coerced when voting)</br>"
+        // + "Confirm Safeword: <input type=\"password\" name=\"confirmsafeword\"/><br>"
+        // + "<input type=\"hidden\" name=\"method\" value=\"register\"/>"
+        // + "<input type=\"submit\" value=\"Submit\"/>";
+
+        
 }
 
 // preSubmit is only ever called as a result of registering
 function preSubmit() {
     var frm = document.getElementById("form");
-    var pass = frm.children[4].value;
-    var confpass = frm.children[6].value;
-    var sw = frm.children[8].value;
-    var csw = frm.children[10].value;
+    var pass = frm.children[3].value;
+    var confpass = frm.children[5].value;
+    var sw = frm.children[7].value;
+    var csw = frm.children[9].value;
     if ((pass == "" || confpass == "" || sw == "" || csw == "") || pass != confpass || sw != csw) {
         return false;
     }
